2eaa896907155b4583dbce06fc92be70b7436e7a
uirou.j <uirou.j@90eb9760-294a-d889-a769-aba35fa39066>
2010-06-04 12:22:24 +0000

    ・config file 関係を ConfigReader に追い出した ・board.x 以外に ball.x を追加して、default ではそちらを読み込むようにした ・command line 引数に /LoadPacketTarget を追加。現在のところ ball と board の二種類を受け付けて、飛んでいくパケットの default がどちらにするかを決定できるようになった ・config file に LoadPacketTarget = target を追加。上記の /LoadPacketTarget コマンドライン引数と同様のもの。

---
+++ trunk/PackterViewer/PackterViewer/ConfigReader.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+
+namespace PackterViewer
+{
+    class ConfigReader
+    {
+        float defaultScale = 8.0f;
+        string sofTalkPath = "softalk/softalkw.exe";
+        string packterMSGString1 = "<html lang=\"ja\"><body>";
+        string packterMSGString2 = "</body></html>";
+        Nullable<int> tickKeyInputAcceptMicrosecond = null;
+        int msgBoxCloseTimeSec = 1000 * 10; // 10秒
+        string loadPacketTarget = "ball";
+
+        public float DefaultScale
+        {
+            get { return defaultScale; }
+            set { defaultScale = value; }
+        }
+        public string SofTalkPath
+        {
+            get { return sofTalkPath; }
+            set { if (value != null && System.IO.File.Exists(value)) sofTalkPath = value; }
+        }
+        public string PackterMSGString1
+        {
+            get { return packterMSGString1; }
+        }
+        public string PackterMSGString2
+        {
+            get { return packterMSGString2; }
+        }
+        public Nullable<int> TickKeyInputAcceptMicrosecond
+        {
+            get { return tickKeyInputAcceptMicrosecond; }
+        }
+        public int MsgBoxCloseTimeSec
+        {
+            get { return msgBoxCloseTimeSec; }
+        }
+        public string LoadPacketTarget
+        {
+            get { return loadPacketTarget; }
+        }
+
+        public void ParseArgs(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                float f;
+                if (args.Length > i + 1 && args[i] == "/size" && float.TryParse(args[i + 1], out f))
+                {
+                    if (f > 0)
+                        defaultScale = f;
+                    i++; continue;
+                }
+
+                if (args.Length > i + 1 && args[i] == "/softalk" && System.IO.File.Exists(args[i + 1]))
+                {
+                    sofTalkPath = args[i + 1];
+                    i++; continue;
+                }
+
+                if (args.Length > i + 1 && args[i] == "/loadPacketTarget")
+                {
+                    SetLoadPacketTarget(args[i + 1]);
+                    i++; continue;
+                }
+
+                if (System.IO.File.Exists(args[i]))
+                {
+                    ReadFromFile(args[i]);
+                    continue;
+                }
+            }
+        }
+        
+        public bool ReadFromFile(string fileName)
+        {
+            try
+            {
+                System.IO.StreamReader reader = new System.IO.StreamReader(fileName, System.Text.Encoding.GetEncoding("Shift_JIS"));
+                string text;
+                while ((text = reader.ReadLine()) != null)
+                {
+                    if (text.IndexOf('#') == 0)
+                        continue;
+                    string[] list = text.Split(new char[] { '=' }, 2);
+                    if (list.Length == 2)
+                    {
+                        string key = list[0];
+                        string value = list[1];
+                        key = key.Trim();
+                        key = key.ToLower();
+                        //value.Trim();
+                        switch (key)
+                        {
+                            case "size":
+                                {
+                                    float f;
+                                    if (float.TryParse(value, out f))
+                                    {
+                                        if (f > 0)
+                                            defaultScale = f;
+                                    }
+                                }
+                                break;
+                            case "softalk":
+                                if (System.IO.File.Exists(value))
+                                {
+                                    sofTalkPath = value;
+                                }
+                                break;
+                            case "packtermsgformat1":
+                                packterMSGString1 = value;
+                                break;
+                            case "packtermsgformat2":
+                                packterMSGString2 = value;
+                                break;
+                            case "tickkeyinputacceptmicrosecond":
+                                try
+                                {
+                                    tickKeyInputAcceptMicrosecond = int.Parse(value);
+                                }
+                                catch
+                                {
+                                    tickKeyInputAcceptMicrosecond = null;
+                                }
+                                break;
+                            case "msgboxclosetimesec":
+                                try
+                                {
+                                    int sec = int.Parse(value);
+                                    if (sec > 0)
+                                    {
+                                        msgBoxCloseTimeSec = sec * 1000;
+                                    }
+                                }
+                                catch { }
+                                break;
+                            case "loadpackettarget":
+                                SetLoadPacketTarget(value);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        void SetLoadPacketTarget(string target)
+        {
+            string t = target.ToLower();
+            switch (t)
+            {
+                case "ball":
+                case "board":
+                    loadPacketTarget = t;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
