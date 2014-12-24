b658e2e53d09ea7c245758839c23c87eb02dc175
namaya2hashi <namaya2hashi@90eb9760-294a-d889-a769-aba35fa39066>
2014-12-24 10:05:46 +0000

    added geoip function

--- PackterAgent/src/pt_geoip.h
+++ PackterAgent/src/pt_geoip.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008 Daisuke Miyamoto. All rights reserved.
+ * Copyright (c) 2011 Daisuke Miyamoto. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -24,61 +24,11 @@
  *
  */
 
-#ifndef __PACKTER_H__
-#define __PACKTER_H__
+#ifndef __PACKTER_GEOIP_H__
+#define __PACKTER_GEOIP_H__
 
-#define PACKTER_SNAPLEN	128
-#define PACKTER_BUFSIZ	1024
-#define PACKTER_BUFSIZ_LONG	16384
-#define PACKTER_VIEWER_PORT 11300
-#define PACKTER_TC_PORT 11301
-#define PACKTER_HEADER	"PACKTER\n"
-#define PACKTER_MSG	"PACKTERMSG\n"
-#define PACKTER_SOUND	"PACKTERSOUND\n"
-#define PACKTER_SE	"PACKTERSE\n"
-#define PACKTER_VOICE	"PACKTERVOICE\n"
-#define PACKTER_SKYDOME	"PACKTERSKYDOMETEXTURE\n"
-#define PACKTER_EARTH	"PACKTEARTH\n"
-#define PACKTER_TCP_ACK	0
-#define PACKTER_TCP_SYN	1
-#define PACKTER_TCP_FIN	2
-#define PACKTER_UDP			3
-#define PACKTER_ICMP		4
-#define PACKTER_TCP_ACK6	5
-#define PACKTER_TCP_SYN6	6
-#define PACKTER_TCP_FIN6	7
-#define PACKTER_UDP6			8
-#define PACKTER_ICMP6			9
-
-#define PACKTER_TRUE		1
-#define PACKTER_FALSE		-1
-
-#ifndef NULL_HDRLEN
-#define NULL_HDRLEN	4
-#endif
-
-#ifndef ETHER_HDRLEN
-#define ETHER_HDRLEN    14
-#endif
-
-#ifndef IP_HDRLEN
-#define IP_HDRLEN				20
-#endif
-
-#ifndef IP6_HDRLEN
-#define IP6_HDRLEN			40
-#endif
-
-#ifndef TCP_HDRLEN
-#define TCP_HDRLEN			20
-#endif
-
-#ifndef UDP_HDRLEN			
-#define UDP_HDRLEN			8
-#endif
-
-#ifndef ICMP_MIN_HDRLEN			/* 8 bit for ICMP type, 8 bit for ICMP code */
-#define ICMP_MIN_HDRLEN	2
-#endif
+static const char * _mk_NA(const char *);
+void packter_geoip(char *, char *);
 
 #endif
+
