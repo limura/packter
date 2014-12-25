406ab0381749be98a58a56b9d1c7e006403f163f
namaya2hashi <namaya2hashi@90eb9760-294a-d889-a769-aba35fa39066>
2014-12-25 03:33:52 +0000

    update copyright

--- trunk/PackterAgent/src/pt_replay.h
+++ trunk/PackterAgent/src/pt_replay.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 Daisuke Miyamoto. All rights reserved.
+ * Copyright (c) 2008-15 Project PACKTER. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
